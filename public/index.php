c7ed22e976aec9b3a1fe1b3a73bbad02cb94d108
busy-gnl <vallantjesse@live.com>
2022-10-16 15:20:06 +0200

    quest autoload done

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once '../src/Hello.php';
+$bonjour = new Hello;
+echo $bonjour->talk();
---
+++ src/Hello.php
@@ -0,0 +1,8 @@
+<?php
+class Hello
+{
+    public function talk(): string
+    {
+        return 'Hello World!';
+    }
+}
