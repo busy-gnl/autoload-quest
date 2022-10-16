ee66b0b5666f068d4d8b17db40a0a45e0bbaf4e8
busy-gnl <vallantjesse@live.com>
2022-10-16 17:27:45 +0200

    quest3

--- public/index.php
+++ public/index.php
@@ -3,6 +3,26 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Hello;
+use CowSay\Cow;
 
 $bonjour = new Hello;
 echo $bonjour->talk();
+
+
+
+
+
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+$output = $bessie->say();
+echo $output;
+
+// or just echo the object for direct output
+echo $bessie;
+
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
